581b576257c14f9808951f9516f9a9f332eb40ba
purplepandade <purplepandamediade@gmail.com>
2023-03-20 20:42:25 +0100

    css

--- assets/theme.js
+++ assets/theme.js
@@ -16,22 +16,40 @@
       toggle: function(){
         console.log('hi');
         var CartDrawer = document.querySelector('#CartDrawer');
+        this.init(CartDrawer);
         var hidden = JSON.parse(CartDrawer.getAttribute('aria-hidden'));
         if(hidden){
           CartDrawer.setAttribute('aria-hidden', false);
         }
-
+      },
+      init: function(CartDrawer){
+        var qtys = CartDrawer.querySelectorAll('.qty--wrapper');
+        qtys.forEach((quantity) => {
+          quantity.addEventListener('click', function(event){
+            Theme.helpers.qty(event,quantity,true);
+          })
+        });
       }
+
     },
     sections: {
       Product: function(section){
         var headerHeight = document.querySelector('#shopify-section-header').clientHeight;
         var sectionHeight = window.innerHeight - headerHeight;
-        var productMeta = section.querySelector('[data-meta]');
-        var variantContainer = section.querySelector('[data-variant-container]');
-        productMeta.style.height = sectionHeight - Theme.helpers.convertRemToPixels(4) + 'px';
-        variantContainer.style.height = sectionHeight - Theme.helpers.convertRemToPixels(4)  + 'px';
 
+        if (window.innerWidth > 649){
+          var productMeta = section.querySelector('[data-meta]');
+          var variantContainer = section.querySelector('[data-variant-container]');
+          productMeta.style.height = sectionHeight - Theme.helpers.convertRemToPixels(4) + 'px';
+          variantContainer.style.height = sectionHeight - Theme.helpers.convertRemToPixels(4)  + 'px';  
+        }else{
+          var productMedia = section.querySelector('[data-media]');
+          var flkty = new Flickity( productMedia, {
+            // options
+            cellAlign: 'left',
+            adaptiveHeight: true
+          });
+        }
 
         var stickyATC = section.querySelector('[data-sticky]');
         var form = section.querySelector('form[action="/cart/add"]');
@@ -98,7 +116,31 @@
     helpers: {
       convertRemToPixels: function(rem) {    
         return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
-    } 
+    },
+    qty: function(e,wrapper,ajaxCart){
+      var plus = wrapper.querySelector('.plus');
+      var minus = wrapper.querySelector('.minus');
+      var text = wrapper.querySelector('[data-value]');
+      var current = parseFloat(text.getAttribute('data-value'));
+      if (e.target.closest('.plus')){
+        var newValue = current + 1;
+        text.setAttribute('data-value', newValue);
+        text.textContent = newValue;
+        return;
+      }
+
+      if (e.target.closest('.minus')){
+        if (newValue === 1){
+          return
+        }
+        var newValue = current - 1;
+        text.setAttribute('data-value', newValue);
+        text.textContent = newValue;
+        return;
+      }
+
+
+    }
     }
   };
 
