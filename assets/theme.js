918ec47ff32ba0a5e7f89aa2dc11ea8f65524afb
Laiqa Mohid <55717512+laimoh@users.noreply.github.com>
2023-05-11 15:58:38 +0100

    Proto Wireframe Official -v

--- assets/theme.js
+++ assets/theme.js
@@ -98,7 +98,7 @@ var Theme = {
       const el = document.querySelectorAll('section');
       const options = {
          rootMargin: "0px",
-         threshold: 0.5
+         threshold: 0.7
       }
       const observer = new IntersectionObserver(entries => {
          entries.forEach(entry => {
