30d1215c6eeed8cb648b7f63c37d8550322f4e81
Hasaan@PurplePanda Ecom <hasaan@purplepanda.media>
2023-11-30 21:42:42 +0100

    css

--- assets/shop.js
+++ assets/shop.js
@@ -2,6 +2,15 @@ document.querySelectorAll('.filter-group').forEach((el) => {
    el.addEventListener('click', () => {
       const filterGroup = el.nextElementSibling;
       filterGroup.classList.toggle('grow');
+
+      if (filterGroup.hasAttribute('data-height')){
+         filterGroup.closest('#FilterMobile').style.height = filterGroup.closest('#FilterMobile').scrollHeight - (parseInt(filterGroup.getAttribute('data-height'))) + 'px';
+         filterGroup.removeAttribute('data-height');
+
+      }else{
+         filterGroup.setAttribute('data-height', getComputedStyle(filterGroup).getPropertyValue('height'));
+         filterGroup.closest('#FilterMobile').style.height = filterGroup.closest('#FilterMobile').scrollHeight + 'px';
+      }
    });
 });
 const isMobile = window.innerWidth < 649
